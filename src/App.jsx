1223fe71dcbea8ba06ad54d4f5edb77073e35c24
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-06-24 11:21:31 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,50 @@
-// rrd imports
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+
+// react-router-dom
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom"; // v6...
 
 // pages
-import Home from "./pages/home";
-import About from "./pages/about";
-import Contact from "./pages/contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Faq from "./pages/help/Faq";
+import Form from "./pages/help/Form";
+import PageNotFound from "./pages/PageNotFound";
+import Articles from "./pages/articles/Articles";
 
-//layuts
+// layouts
 import RootLayout from "./layout/RootLayout";
+import ContactLayout from "./layout/ContactLayout";
+import ArticlesLayout from "./layout/ArticlesLayout";
 
-const App = () => {
-  const routers = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: "/about",
-          element: <About />,
-        },
-        {
-          path: "/contact",
-          element: <Contact />,
-        },
-      ],
-    },
-    {},
-  ]);
-  return <RouterProvider router={routers} />;
-};
+function App() {
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<ContactLayout />}>
+          <Route path="faq" element={<Faq />} />
+          <Route path="form" element={<Form />} />
+        </Route>
+        <Route path="articles" element={<ArticlesLayout />}>
+          <Route index element={<Articles />} />
+        </Route>
+        {/* PAGE NOT FOUND */}
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    )
+  );
+
+  return (
+    <div className="App">
+      <RouterProvider router={routes} />
+    </div>
+  );
+}
 
 export default App;
