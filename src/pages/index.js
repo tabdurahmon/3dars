1d825e97f2e770fecd42f479eddb59c2a7a1a328
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-07-02 10:05:20 +0500

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
-// fayllar
-
-export { default as Home } from "./Home";
-
-export { default as Contact } from "./Contact";
-export { default as Login } from "./Login";
-export { default as Register } from "./Register";
-export { default as ErrorPage } from "./ErrorPage";
-export { default as About } from "./About";
+export { default as Home } from "./Home.jsx";
+export { default as Contact } from "./Contact.jsx";
+export { default as Login } from "./Login.jsx";
+export { default as Register } from "./Register.jsx";
+export { default as ErrorPage } from "./ErrorPage.jsx";
+export { default as About } from "./About.jsx";
